9d71b584a3f45e195e917bf77f9140240d96bafe
Teddy Kalp <teddy.kalp@ucalgary.ca>
2020-10-27 15:47:02 -0600

    added location manager to use for user's current location

--- YYCFoodTrucks/Managers/LocationManager.swift
+++ YYCFoodTrucks/Managers/LocationManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import MapKit
+
+
+class LocationManager: NSObject, ObservableObject{
+
+    private let locationManager = CLLocationManager()
+    @Published var location: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        self.location = location
+    }
+    
+}
