7b81546568e1d0578d60271dc4abf7a0d34e47e4
teddy-kalp <71520346+teddy-kalp@users.noreply.github.com>
2020-10-27 15:03:30 -0600

    Update LandMark.swift

--- YYCFoodTrucks/Model/LandMark.swift
+++ YYCFoodTrucks/Model/LandMark.swift
@@ -10,13 +10,6 @@ import CoreLocation
 
 
 class LandMark: Identifiable, Codable {
-<<<<<<< HEAD
-    var name: String
-    var address: String
-    var coords: [Int]
-}
-
-=======
     var address: String
     var latitude: Double
     var longitude: Double
@@ -32,7 +25,6 @@ class LandMark: Identifiable, Codable {
 }
 
 
->>>>>>> e6abc10205599d158c4af91cecdd71c28f1c1920
 // used to convert addresses to lats and longs -> still trying to get to work
 func convertToCoords(address: String) -> (CLLocationDegrees, CLLocationDegrees){
     let geocoder = CLGeocoder()
@@ -41,15 +33,6 @@ func convertToCoords(address: String) -> (CLLocationDegrees, CLLocationDegrees){
     geocoder.geocodeAddressString(address){
         placemarks, error in
         let placemark = placemarks?.first
-<<<<<<< HEAD
-        lat = placemark?.location?.coordinate.latitude ?? 0
-        lon = placemark?.location?.coordinate.longitude ?? 0
-    }
-
-    return (lat, lon)
-}
-
-=======
         print(error)
         lat = placemark?.location?.coordinate.latitude ?? 0
         print("Lat", placemark?.location?.coordinate.latitude)
@@ -61,7 +44,7 @@ func convertToCoords(address: String) -> (CLLocationDegrees, CLLocationDegrees){
 }
 
 
->>>>>>> e6abc10205599d158c4af91cecdd71c28f1c1920
+
 //let Locations = [
 //    LandMark(name: "Location 1", address: "2500 University Avenue, Calgary, AB T2N1N4", coords: (51.076340, -114.131520)),
 //    LandMark(name: "Location 2", address: "819 4 Avenue NW, Calgary, AB", coords: (51.057700, -114.082170)),
@@ -71,8 +54,3 @@ func convertToCoords(address: String) -> (CLLocationDegrees, CLLocationDegrees){
 
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> e6abc10205599d158c4af91cecdd71c28f1c1920
