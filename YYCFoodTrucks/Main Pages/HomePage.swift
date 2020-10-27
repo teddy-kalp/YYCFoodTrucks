3931edfef5371a6c5dedf4287f942fb5beabc96c
teddy-kalp <71520346+teddy-kalp@users.noreply.github.com>
2020-10-27 15:01:07 -0600

    Update HomePage.swift

--- YYCFoodTrucks/Main Pages/HomePage.swift
+++ YYCFoodTrucks/Main Pages/HomePage.swift
@@ -5,20 +5,6 @@
 //  Created by Teddy Kalp on 2020-10-16.
 //
 
-import SwiftUI
-<<<<<<< HEAD
-
-
-struct HomePage: View {
-    var body: some View {
-            VStack {
-                yycHeader()
-                MapView()
-                    .frame(width: 370, height: 650, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-                NavBar(map: true, discover: false, favorite: false, events: false)
-                }
-=======
 import MapKit
 
 
