28e80fd48819f6326f65797c73f428c48ff90679
Teddy Kalp <teddy.kalp@ucalgary.ca>
2020-10-27 16:52:41 -0600

    added different colored annotations to map to signify if open or closed trucks

--- YYCFoodTrucks/Main Pages/HomePage.swift
+++ YYCFoodTrucks/Main Pages/HomePage.swift
@@ -6,16 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 
 struct HomePage: View {
     @EnvironmentObject var router: Router
+    @ObservedObject var TruckRepo = TruckRespository()
+    @ObservedObject var LocationRepo = LandMarkRespository()
+    @ObservedObject var ScheduleRepo = ScheduleRespository()
+    
     var body: some View {
             VStack {
                 yycHeader()
                 if router.cur_page == "HomePage"{
                     GeometryReader { geometry in
-                    MapView()
+                        MapView(annotations: getAnnotations())
                         .frame(width: geometry.size.width, height: geometry.size.height - 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         .border(Color.gray)
                     }
@@ -32,6 +37,58 @@ struct HomePage: View {
                 }
             }
         }
+    
+    func getAnnotations() -> [TruckAnnotation]{
+        var truckAnnotations = [TruckAnnotation]()
+        for schedule in ScheduleRepo.schedules{
+            if (schedule.closeDate < Date()){
+                continue
+            }
+            else{
+                var foundTruck: Truck?
+                var location: LandMark?
+                for tr in TruckRepo.trucks{
+                    if (tr.id == schedule.truckId){
+                        foundTruck = tr
+                        break
+                    }
+                }
+                for lc in LocationRepo.landmarks{
+                    if (lc.locationId == schedule.locationId){
+                        location = lc
+                        break
+                    }
+                }
+                let truckAnnotation = TruckAnnotation(truck: foundTruck!)
+                truckAnnotation.coordinate = CLLocationCoordinate2D(latitude: location!.latitude, longitude: location!.longitude)
+                truckAnnotation.title = foundTruck!.name
+                // different formators for the dates
+                let hoursMinutes = DateFormatter()
+                hoursMinutes.dateFormat = "HH:MM"
+                // different formators for the dates
+                let monthDayYear = DateFormatter()
+                monthDayYear.dateFormat = "MMM dd,yyyy"
+                
+                let openTime = hoursMinutes.string(from: schedule.openDate)
+                let closeTime = hoursMinutes.string(from: schedule.closeDate)
+                let openDate = monthDayYear.string(from: schedule.openDate)
+                //let closeDate = monthDayYear.string(from: schedule.closeDate)
+                
+                
+                if (schedule.openDate < Date() && Date() < schedule.closeDate){
+                    truckAnnotation.subtitle = "Open Now! Closes at \(closeTime)"
+                    truckAnnotation.truck.open = false
+                }
+                else if (schedule.openDate > Date()){
+                    truckAnnotation.subtitle = "Opens Soon! \(openDate) from \(openTime) to \(closeTime)"
+                    truckAnnotation.truck.open = true
+                }
+                
+                truckAnnotations.append(truckAnnotation);
+            }
+        }
+        return truckAnnotations;
+    }
 }
     
 
